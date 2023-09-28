97d8d65602da57e8a08b716e02f69592cf0df6cf
COUGAR <COUGAR@gmail.com>
2023-09-28 19:35:44 +0200

    first steps towards stripe webhooks

---
+++ pages/api/webhooks.ts
@@ -0,0 +1,28 @@
+// const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+// const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    console.log(req);
+
+    // const buf = await buffer(req);
+    // const sig = req.headers['stripe-signature'];
+
+    // let event;
+
+    try {
+      // event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err) {
+      // res.status(400).send(`Webhook Error: ${err.message}`);
+      // return;
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+};
+
+export default handler;
