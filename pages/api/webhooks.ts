54bd546f9e9be1a39e74db77be15214c3d9ee6ee
COUGAR <COUGAR@gmail.com>
2023-09-29 15:48:57 +0200

    uhuhuhu

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -11,7 +11,7 @@ const handler = async (req: any, res: any) => {
 
     // let event;
 
-    const listLineItems = stripe.checkout.sessions.listLineItems(
+    const listLineItems = await stripe.checkout.sessions.listLineItems(
       req.body.data.object.id,
       function (err: any, lineItems: any) {
         return lineItems;
@@ -36,3 +36,85 @@ const handler = async (req: any, res: any) => {
 };
 
 export default handler;
+
+// de: false,
+//       locale: null,
+//       metadata: {},
+//       mode: 'payment',
+//       payment_intent: 'pi_3Nvgz0G8O1OemN5V1SEpJe4e',
+//       payment_link: null,
+//       payment_method_collection: 'if_required',
+//       payment_method_configuration_details: null,
+//       payment_method_options: {},
+//       payment_method_types: [Array],
+//       payment_status: 'paid',
+//       phone_number_collection: [Object],
+//       recovered_from: null,
+//       setup_intent: null,
+//       shipping_address_collection: null,
+//       shipping_cost: [Object],
+//       shipping_details: null,
+//       shipping_options: [Array],
+//       status: 'complete',
+//       submit_type: 'pay',
+//       subscription: null,
+//       success_url: 'https://ecommerce-app-liart-five.vercel.app/?success=true',
+//       total_details: [Object],
+//       url: null
+//     }
+//   },
+//   livemode: false,
+//   pending_webhooks: 1,
+//   request: { id: null, idempotency_key: null },
+//   type: 'checkout.session.completed'
+// }
+// LINE ITEM:  Promise {
+//   <pending>,
+//   autoPagingEach: [Function: autoPagingEach],
+//   autoPagingToArray: [Function: autoPagingToArray],
+//   next: [Function: next],
+//   return: [Function: return],
+//   [Symbol(async_id_symbol)]: 304,
+//   [Symbol(trigger_async_id_symbol)]: 302,
+//   [Symbol(kResourceStore)]: {
+//     requestId: '1',
+//     serverId: '1f23a0a4-e497-48ac-829c-0f682f1a2f2f',
+//     n1RequestId: undefined,
+//     vercelId: 'pdx1::9kxl5-1695995036240-6efc6844a5dd'
+//   },
+//   [Symbol(kResourceStore)]: {
+//     headers: {
+//       host: 'ecommerce-app-liart-five.vercel.app',
+//       'content-type': 'application/json; charset=utf-8',
+//       'x-real-ip': '54.187.174.169',
+//       'x-vercel-proxy-signature-ts': '1695995336',
+//       'x-vercel-deployment-url': 'ecommerce-ldoxesmwa-lgilski.vercel.app',
+//       'cache-control': 'no-cache',
+//       'x-vercel-sc-basepath': '',
+//       'x-vercel-ip-latitude': '45.8234',
+//       'x-vercel-forwarded-for': '54.187.174.169',
+//       'x-vercel-sc-host': 'iad1.suspense-cache.vercel-infra.com',
+//       'x-vercel-sc-headers': '{"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXBsb3ltZW50SWQiOiJkcGxfRkQ3ckdQQ2ZDcTFRRjV2YWdFTlYyaVZtMzg4WSIsInVubGltaXRlZCI6ZmFsc2UsInBsYW4iOiJob2JieSIsImRvbWFpbiI6ImVjb21tZXJjZS1hcHAtbGlhcnQtZml2ZS52ZXJjZWwuYXBwIiwiYmxvY2siOmZhbHNlLCJpYXQiOjE2OTU5OTUwMzYsInByb2plY3RJZCI6InByal9Ua0VkTFEwcXNEMk40WWw5U0Q3OTFqVWlLZUFOIiwiZXhwIjoxNjk1OTk1OTU2LCJvd25lcklkIjoiaTRWVTJnODBwQk0zdlhobUNjQ1o0YjZSIiwicmVxdWVzdElkIjoiOWt4bDUtMTY5NTk5NTAzNjI0MC02ZWZjNjg0NGE1ZGQiLCJlbnYiOiJwcm9kdWN0aW9uIn0.kzdSI5mf0jx-s9OYZQ9jlmUBWUHmF4LpE_JW2vvt2_Y"}',
+//       forwarded: 'for=54.187.174.169;host=ecommerce-app-liart-five.vercel.app;proto=https;sig=0QmVhcmVyIDc4NWFmZjczMmQ4NGU0MmNlNjE5ZGI0ZTI2Mzc1NWRjMjhkNjg5Y2NmOWZmMjc5ZDExYTcxZmIwNWEyYzAxOWQ=;exp=1695995336',
+//       'x-vercel-id': 'pdx1::9kxl5-1695995036240-6efc6844a5dd',
+//       'content-length': '3356',
+//       'x-matched-path': '/api/webhooks',
+//       'x-vercel-ip-longitude': '-119.7257',
+//       accept: '*/*; q=0.5, application/xml',
+//       'x-forwarded-host': 'ecommerce-app-liart-five.vercel.app',
+//       'stripe-signature': 't=1695995036,v1=aeee2d554f29e3848cf9dc899ed025f04263de8cbe086719f758719dd104be4b,v0=8ca6d1e6bf29305d0b551582a3d2dcd6a763d2af21008475ada953975c6d4b55',
+//       'x-vercel-ip-country': 'US',
+//       'x-vercel-ip-country-region': 'OR',
+//       'x-vercel-proxy-signature': 'Bearer 785aff732d84e42ce619db4e263755dc28d689ccf9ff279d11a71fb05a2c019d',
+//       'x-forwarded-for': '54.187.174.169',
+//       'user-agent': 'Stripe/1.0 (+https://stripe.com/docs/webhooks)',
+//       'x-vercel-ip-timezone': 'America/Los_Angeles',
+//       'x-vercel-ip-city': 'Boardman',
+//       'x-forwarded-proto': 'https',
+//       'x-vercel-proxied-for': '54.187.174.169',
+//       connection: 'close'
+//     },
+//     url: 'https://ecommerce-app-liart-five.vercel.app/api/webhooks'
+//   },
+//   [Symbol(Symbol.asyncIterator)]: [Function: [Symbol.asyncIterator]]
+}
