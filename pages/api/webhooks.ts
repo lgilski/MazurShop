e08b04b540ec8a7e45a082eba435fbf6407b81b3
COUGAR <COUGAR@gmail.com>
2023-09-29 15:50:18 +0200

    ahhhahaha

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -28,7 +28,7 @@ const handler = async (req: any, res: any) => {
       return;
     }
 
-    res.json({ received: true, data: listLineItems });
+    res.json({ received: true, data: await listLineItems });
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
