60f54ae6b392cbfb05876c8a9bde5b342541e2d1
COUGAR <COUGAR@gmail.com>
2023-09-29 13:55:48 +0200

    aaaaaaaaa

--- helpers/getStripe.ts
+++ helpers/getStripe.ts
@@ -1,10 +1,10 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-let stripePromise;
+let stripePromise: any;
 
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   }
 
   return stripePromise;
