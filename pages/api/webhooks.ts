732bc8cc44d840ecf40c1b463d318d74a9ef3e88
COUGAR <COUGAR@gmail.com>
2024-05-21 21:00:21 +0200

    fix???????

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -13,8 +13,7 @@ export async function updateDocumentLeftInStock(_id: string, quantity: number) {
 }
 
 const handler = async (req: any, res: any) => {
-  console.log(req);
-  if (req.type === 'checkout.session.completed') {
+  if (req.method === 'POST') {
     try {
       const listLineItems = req.body.data?.object?.id
         ? await stripe.checkout.sessions.listLineItems(
