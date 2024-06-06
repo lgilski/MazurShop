d5bdc894ed2389d44111977fbf3ff871821c8e5a
COUGAR <COUGAR@gmail.com>
2024-06-06 15:33:33 +0200

    a

--- components/Products/Product.tsx
+++ components/Products/Product.tsx
@@ -62,6 +62,8 @@ export default function Product({ product }: { product: ProductType }) {
     }
   };
 
+  // a
+
   useEffect(() => {
     const elements = JSON.parse(localStorage.getItem('lastSeen') || '{}');
     if (elements) {
