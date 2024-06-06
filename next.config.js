b101d2a995d946140eb363505d3d976253369d52
COUGAR <COUGAR@gmail.com>
2024-06-06 17:15:25 +0200

    propably final fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['cdn.sanity.io'] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
