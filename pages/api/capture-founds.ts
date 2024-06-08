30343344ce895ab4b968c860dbe0e139d3d0d375
COUGAR <COUGAR@gmail.com>
2024-06-08 16:34:56 +0200

    step 10

--- pages/api/capture-founds.ts
+++ pages/api/capture-founds.ts
@@ -33,7 +33,7 @@ export default async function captureFounds(req: any, res: any) {
 
       const areItemsAvailable =
         listItems &&
-        listItems?.data.forEach(async (boughtItem: any) => {
+        listItems?.data.map(async (boughtItem: any) => {
           const boughtItemData = data.find(
             (product: any) => product.name === boughtItem.description
           );
