c7c33f4802ae0baa6785c992c35bb00a84d050f7
COUGAR <COUGAR@gmail.com>
2024-06-08 14:05:01 +0200

    added console.log

--- pages/api/capture-founds.ts
+++ pages/api/capture-founds.ts
@@ -10,6 +10,8 @@ export default async function captureFounds(req: any, res: any) {
         limit: 3,
       });
 
+      console.log(sessions);
+
       const paymentIntentCheckout = await sessions.data.find(
         (session: any) => session.payment_intent === req.data.object.id
       );
