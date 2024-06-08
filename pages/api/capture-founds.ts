13a9a59f64ca282b99362b352ec301bb3577de88
COUGAR <COUGAR@gmail.com>
2024-06-08 16:05:12 +0200

    aaaaa

--- pages/api/capture-founds.ts
+++ pages/api/capture-founds.ts
@@ -24,10 +24,10 @@ export default async function captureFounds(req: any, res: any) {
       // console.log('paymentIntentCheckout: ', paymentIntentCheckout);
       console.log('paymentIntentCheckout: ', paymentIntentCheckout.id);
 
-      // const listLineItems = req.body.data?.object?.id
-      //   ? stripe.checkout.session.listLineItems(paymentIntentCheckout.id)
-      //   : null;
-      // console.log('listLineItems:', listLineItems);
+      const listLineItems = req.body.data?.object?.id
+        ? await stripe.checkout.session.listLineItems(paymentIntentCheckout.id)
+        : null;
+      console.log('listLineItems:', listLineItems);
 
       // const data = await clientRead.fetch(productsDetailsQuery);
 
