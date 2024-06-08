db77eac5922f76562aa35bff8c6324e3ce7ccd8f
COUGAR <COUGAR@gmail.com>
2024-06-08 13:55:23 +0200

    another implementation

--- pages/api/capture-founds.ts
+++ pages/api/capture-founds.ts
@@ -6,9 +6,20 @@ const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 export default async function captureFounds(req: any, res: any) {
   if (req.method === 'POST') {
     try {
+      const sessions = await stripe.checkout.sessions.list({
+        limit: 3,
+      });
+
+      const paymentIntentCheckout = await sessions.data.find(
+        (session: any) => session.payment_intent === req.data.object.id
+      );
+
+      console.log('paymentIntentCheckout: ', paymentIntentCheckout);
+
       const listLineItems = req.body.data?.object?.id
-        ? await stripe.paymentIntents.listLineItems(req.body.data?.object?.id)
+        ? stripe.checkout.session.listLineItems(paymentIntentCheckout.data?.id)
         : null;
+      console.log('listLineItems:', listLineItems);
 
       const data = await clientRead.fetch(productsDetailsQuery);
 
@@ -21,6 +32,8 @@ export default async function captureFounds(req: any, res: any) {
 
           return boughtItemData.leftInStock - boughtItem.qantity > 0;
         });
+
+      console.log('areItemsAvailable:', areItemsAvailable);
 
       if (areItemsAvailable.every((element: any) => element === true)) {
         await stripe.paymentIntents.capture(req.id, {
