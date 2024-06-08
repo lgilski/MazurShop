994de68f80a70acb29b4f507b1a65e8e7cb33f2b
COUGAR <COUGAR@gmail.com>
2024-06-08 15:36:05 +0200

    aaaaa

--- pages/api/capture-founds.ts
+++ pages/api/capture-founds.ts
@@ -10,7 +10,7 @@ export default async function captureFounds(req: any, res: any) {
         limit: 2,
       });
 
-      console.log('Req object: ', req.body.object);
+      console.log('Req object: ', req.body.json());
 
       console.log('Sessions: ', sessions.data);
 
