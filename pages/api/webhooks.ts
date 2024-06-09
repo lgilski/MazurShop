f2ebc9e3c88c96872b60830c03f2cd16c91133a9
COUGAR <COUGAR@gmail.com>
2024-06-09 09:14:41 +0200

    change

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -3,28 +3,28 @@ import { client, clientRead } from '@/sanity/lib/client';
 
 const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
-export async function updateDocumentLeftInStock(_id: string, quantity: number) {
-  const result = await client
-    .patch(_id)
-    .dec({ leftInStock: quantity })
-    .commit();
+// export async function updateDocumentLeftInStock(_id: string, quantity: number) {
+//   const result = await client
+//     .patch(_id)
+//     .dec({ leftInStock: quantity })
+//     .commit();
 
-  console.log('Updated!! ', _id, quantity);
+//   console.log('Updated!! ', _id, quantity);
 
-  return result;
-}
+//   return result;
+// }
 
 const handler = async (req: any, res: any) => {
   if (req.method === 'POST') {
-    const sessions = await stripe.checkout.sessions.list({
-      limit: 15,
-    });
-
-    const paymentIntentCheckout = sessions.data.find(
-      (session: any) => session.payment_intent === req.body.data?.object.id
-    );
-
     try {
+      const sessions = await stripe.checkout.sessions.list({
+        limit: 15,
+      });
+
+      const paymentIntentCheckout = sessions.data.find(
+        (session: any) => session.payment_intent === req.body.data?.object.id
+      );
+
       const listLineItems = paymentIntentCheckout
         ? await stripe.checkout.sessions.listLineItems(paymentIntentCheckout.id)
         : null;
@@ -66,39 +66,3 @@ const handler = async (req: any, res: any) => {
 };
 
 export default handler;
-
-// Updated!!  192f82dd-2ad0-409b-8002-e2f27896477c 5
-// TypeError: Cannot read properties of undefined (reading 'object')
-// at /var/task/.next/server/pages/api/webhooks.js:7:596
-// at Array.find (<anonymous>)
-// at c (/var/task/.next/server/pages/api/webhooks.js:7:557)
-// at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
-// at async K (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:16853)
-// at async U.render (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:17492)
-// at async r3.runApi (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:43997)
-// at async r3.handleCatchallRenderRequest (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:37953)
-// at async r3.runImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:17465)
-// at async r3.handleRequestImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:16554)
-// ⨯ TypeError: Cannot read properties of undefined (reading 'object')
-// at /var/task/.next/server/pages/api/webhooks.js:7:596
-// at Array.find (<anonymous>)
-// at c (/var/task/.next/server/pages/api/webhooks.js:7:557)
-// at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
-// at async K (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:16853)
-// at async U.render (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:17492)
-// at async r3.runApi (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:43997)
-// at async r3.handleCatchallRenderRequest (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:37953)
-// at async r3.runImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:17465)
-// at async r3.handleRequestImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:16554)
-// TypeError: Cannot read properties of undefined (reading 'object')
-// at /var/task/.next/server/pages/api/webhooks.js:7:596
-// at Array.find (<anonymous>)
-// at c (/var/task/.next/server/pages/api/webhooks.js:7:557)
-// at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
-// at async K (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:16853)
-// at async U.render (/var/task/node_modules/next/dist/compiled/next-server/pages-api.runtime.prod.js:20:17492)
-// at async r3.runApi (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:43997)
-// at async r3.handleCatchallRenderRequest (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:17:37953)
-// at async r3.runImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:17465)
-// at async r3.handleRequestImpl (/var/task/node_modules/next/dist/compiled/next-server/server.runtime.prod.js:16:16554)
-// Node.js process exited with exit status: 1. The logs above can help with debugging the issue.
